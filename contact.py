f8f2126b5e976a79d2188f737f5ce62a219ba74c
electrosvoboda <51613897+electrosvoboda@users.noreply.github.com>
2020-06-12 01:44:39 +0300

    Create new test for contact
    Создан новый тест для создания контакта. Выделен вспомогательный класс

---
+++ contact.py
@@ -0,0 +1,17 @@
+class Contact:
+    def __init__(self, firstname, lastname, nickname, company,
+                 address, homephone, mobilephone, workphone,
+                 fax, email, bday, bmonth, byear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
