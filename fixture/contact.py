8058637709e00034bfad27822cb438f3a4ca1969
electrosvoboda <51613897+electrosvoboda@users.noreply.github.com>
2020-06-17 01:02:59 +0300

    Выделены вспомогательные классы
    Выделены вспомогательные классы для работы с группами и контактами

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,30 +1,12 @@
-from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 
-class Application2:
-
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+class ContactHelper:
+    def __init__(self, app):
+        self.app = app
 
     def create_contact(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         # инициация создание нового контакта
         wd.find_element_by_link_text("add new").click()
         # ввод данных клиента
@@ -75,13 +57,6 @@ class Application2:
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_home_page()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
     def return_home_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("home").click()
-
-    def destroy(self):
-        self.wd.quit()
--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from fixture.application2 import Application2
+from fixture.application import Application
 import pytest
 
 
 @pytest.fixture
-def app2(request):
-    fixture = Application2()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
-def test_create_contact(app2):
-    app2.login(username="admin", password="secret")
-    app2.create_contact(Contact(firstname="keks", lastname="uffff", nickname="xxxxxxxxm", company="psss",
+def test_create_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(Contact(firstname="keks", lastname="uffff", nickname="xxxxxxxxm", company="psss",
                                 address="cosmos", homephone="+79990124563", mobilephone="+74952149695",
                                 workphone="+79883961245", fax="+79004523625", email="agrrhzerh@mail.ru",
                                 bday="7", bmonth="July", byear="1999"))
-    app2.logout()
+    app.session.logout()
