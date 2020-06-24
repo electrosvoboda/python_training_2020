e9c76c8c14bcb5032a13b928b8195c42379b5932
electrosvoboda <51613897+electrosvoboda@users.noreply.github.com>
2020-06-25 00:32:39 +0300

    Выполнена проверка валидности фикстуры
    Сделан интеллектуальный login и logout

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_create_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="keks", lastname="uffff", nickname="xxxxxxxxm", company="psss",
                                 address="cosmos", homephone="+79990124563", mobilephone="+74952149695",
                                 workphone="+79883961245", fax="+79004523625", email="agrrhzerh@mail.ru",
                                 byear="1999"))
-    app.session.logout()
--- test/test_modify_first_contact.py
+++ test/test_modify_first_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="cherry"))
-    app.session.logout()
 
 
 def test_modify_contact_nickname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(nickname="bomb"))
-    app.session.logout()
