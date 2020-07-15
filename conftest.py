28d47a03de1eb392d112cc7bddcc9f6128b9192e
electrosvoboda <51613897+electrosvoboda@users.noreply.github.com>
2020-07-15 22:49:48 +0300

    Исправлена ошибка в запуске с консоли

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ fixture = None
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
         fixture = Application(browser=browser, base_url=base_url)
     else:
